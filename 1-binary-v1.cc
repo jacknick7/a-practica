4ad9a06a9cc8b1a7d77847095731e748178776c8
jacknick7 <josep.ciurana@est.fib.upc.edu>
2018-03-24 18:51:33 +0100

    Menja Merda

--- 1-binary-v1.cc
+++ 1-binary-v1.cc
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include "1-binary-v1.hh"
 using namespace std;
 
+
+vector<int> dic, text;
+
+
+void mergeSort(vector<int>& dic) {
+    int mig = dic.size()/2;
+    vector<int> dicf(mig);
+    vector<int> dicl(dic.size()-mig);
+}
+
+
+
+
+
+void binaryV1(vector<int>& dict, vector<int>& entr) {
+    dic = dict;
+    text = entr;
+}
+
 int main() {
-    ifstream File;
-    File.open("exemples/exemple_01.dict");
-    if (not File.is_open())
-        cout << "No s'ha pogut llegir l'exemple_01.dict" << endl;
-    int n;
-    File >> n;
-    vector<int> dicData(n), text(2*n);
-    for (int i = 0; i < n; ++i) File >> dicData[i];
-    for (int i = 0; i < n; ++i) cout << dicData[i] << endl;
-    cout << dicData[0] << ' ' << dicData[n-1] << endl;
-    File.close();
-    File.open("exemples/exemple_01.entr");
-    if (not File.is_open())
-        cout << "No s'ha pogut llegir l'exemple_01.entr" << endl;
-    for (int i = 0; i < 2*n; ++i) File >> text[i];
-    for (int i = 0; i < 2*n; ++i) cout << text[i] << endl;
-    cout << text[0] << ' ' << text[2*n-1] << endl;
-    File.close();
+    binaryV1();
 }
