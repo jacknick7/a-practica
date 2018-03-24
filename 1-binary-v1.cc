c013793e1c5bade0be0caadf02ae4bde942e775b
oriolclosa <oriol.closa@est.fib.upc.edu>
2018-03-24 18:21:30 +0100

    Canvis

--- 1-binary-v1.cc
+++ 1-binary-v1.cc
@@ -4,12 +4,11 @@
 
 using namespace std;
 
-
 int main() {
     ifstream File;
-    File.open("arxiu1.txt");
+    File.open("exemples/exemple_01.dict");
     if (not File.is_open())
-        cout << "No s'ha pogut llegir l'arxiu1" << endl;
+        cout << "No s'ha pogut llegir l'exemple_01.dict" << endl;
     int n;
     File >> n;
     vector<int> dicData(n), text(2*n);
@@ -17,9 +16,9 @@ int main() {
     for (int i = 0; i < n; ++i) cout << dicData[i] << endl;
     cout << dicData[0] << ' ' << dicData[n-1] << endl;
     File.close();
-    File.open("arxiu2.txt");
+    File.open("exemples/exemple_01.entr");
     if (not File.is_open())
-        cout << "No s'ha pogut llegir l'arxiu2" << endl;
+        cout << "No s'ha pogut llegir l'exemple_01.entr" << endl;
     for (int i = 0; i < 2*n; ++i) File >> text[i];
     for (int i = 0; i < 2*n; ++i) cout << text[i] << endl;
     cout << text[0] << ' ' << text[2*n-1] << endl;
