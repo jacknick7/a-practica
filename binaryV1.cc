c4f07b8d3a1181596fe028a0a22ddd401b770104
jacknick7 <josep.ciurana@est.fib.upc.edu>
2018-03-26 20:20:39 +0200

    Mergsort fet, no va per alguna rao

--- binaryV1.cc
+++ binaryV1.cc
@@ -4,13 +4,48 @@ using namespace std;
 
 vector<int> dic, text;
 
-void mergeSort(vector<int>& dic){
-    int mig = dic.size()/2;
-    vector<int> dicf(mig);
-    vector<int> dicl(dic.size()-mig);
+/* Pre: --
+ * Post: la funcio retorna el vector original ordenat usant merge sort (cost nlogn) 
+ */
+vector<int> mergeSort(vector<int> pdic) {
+    int n = pdic.size();
+    if (n > 1) {
+        int m = n/2;
+        vector<int> dicf(m);
+        vector<int> dicl(n-m);
+        for (int i = 0; i <= m; ++i) dicf[i] = pdic[i];
+        for (int i = 0; i < dicl.size(); ++i) dicl[i] = pdic[i+m];
+        vector<int> me = mergeSort(dicf);
+        vector<int> md = mergeSort(dicl);
+        int i, j, k;
+        i = j = k = 0;
+        while (j != me.size() and k != md.size()) {
+            if (me[j] < md[k]) {
+                pdic[i] = me[j];
+                ++j;
+            }
+            else {
+                pdic[i] = md[k];
+                ++k;
+            }
+            ++i;
+        }
+        while (j != me.size()) {
+            pdic[i] = me[j];
+            ++j;
+            ++i;
+        }
+        while (k != md.size()) {
+            pdic[i] = md[k];
+            ++k;
+            ++i;
+        }
+    }
+    return pdic;
 }
 
+
 void binaryV1(vector<int>& dict, vector<int>& entr){
-    dic = dict;
+    dic = mergeSort(dict);
     text = entr;
 }
