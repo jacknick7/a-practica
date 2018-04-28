3e7c1b83392663279a0456d365597ca2194ec6e5
jacknick7 <josep.ciurana@est.fib.upc.edu>
2018-04-28 11:47:31 +0200

    bloomV1 aka Perfection completat

--- bloomV1.cc
+++ bloomV1.cc
@@ -1,26 +1,30 @@
 #include "bloomV1.hh"
 #include <ctime>
 
+// Els bools en C++ ocupen 1 byte per tenir @ optima,
+// en futures versions usar setbit
 vector<bool> bloomTab;
+int desp;
 
 int hashF1(int x) {
-    return (3 * x + 3 % 6) % bloomTab.size(); 
+    return x % desp; 
 }
 
 int hashF2(int x) {
-    return (2 * x + 9 % 2) % bloomTab.size();
+    return ((x + 1) % (bloomTab.size() - desp)) + desp;
 }
 
 int hashF3(int x) {
-    return (3 * x + 7 % 8) % bloomTab.size();
+    return (((x % 37) + x) % (bloomTab.size() - 2 * desp)) + 2 * desp;
 }
 
 int hashF4(int x) {
-    return (2 * x + 3 % 5) % bloomTab.size();
+    return ((x * 7) % (bloomTab.size() - 3 * desp)) + 3 * desp;
 }
 
 void makeTable(vector<int>& dict) {
     bloomTab = vector<bool> (dict.size() * 16, false);
+    desp = 4 * dict.size();
     for (auto elem : dict) {
         bloomTab[hashF1(elem)] = true;
         bloomTab[hashF2(elem)] = true;
@@ -43,10 +47,23 @@ void bloomV1(vector<int>& dict, vector<int>& entr){
     cout << "Temps per generar la taula de bloom: " << (stop1-start1)/double(CLOCKS_PER_SEC)*1000 << endl;
     
     /* Cerca al conjunt */
+    vector<int> posibles;
     int start2 = clock();
     for (auto elem : entr) {
-        if (maybe(elem)) cout << elem << endl;
+        if (maybe(elem)) posibles.push_back(elem);
     }
     int stop2 = clock();
+    int falsPositiu = 0;
+    for (auto elem : posibles) {
+        int i = 0;
+        bool trobat = false;
+        while (i < dict.size() and not trobat) {
+            if (elem == dict[i]) trobat = true;
+            ++i;
+        }
+        if (not trobat) ++falsPositiu;
+    }
     cout << "Temps per fer totes les cerques: " << (stop2-start2)/double(CLOCKS_PER_SEC)*1000 << endl;
+    cout << "Numero de fals positius: " << falsPositiu << endl;
+    cout << "Numero total de positius: " << posibles.size() << endl;
 }
--- main.cc
+++ main.cc
@@ -11,17 +11,17 @@ using namespace std;
 
 int main() {
     ifstream File;
-    File.open("exemples/exemple_02.dict");
+    File.open("exemples/exemple_01.dict");
     if (not File.is_open())
-        cout << "No s'ha pogut llegir l'exemple_02.dict." << endl;
+        cout << "No s'ha pogut llegir l'exemple_01.dict." << endl;
     int n;
     File >> n;
     vector<int> dict(n), entr(2*n);
     for (int i = 0; i < n; ++i) File >> dict[i];
     File.close();
-    File.open("exemples/exemple_02.entr");
+    File.open("exemples/exemple_01.entr");
     if (not File.is_open())
-        cout << "No s'ha pogut llegir l'exemple_02.entr." << endl;
+        cout << "No s'ha pogut llegir l'exemple_01.entr." << endl;
     for (int i = 0; i < 2*n; ++i) File >> entr[i];
     File.close();
     
