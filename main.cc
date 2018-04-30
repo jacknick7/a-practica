ba2c4f02859d3bdd34c33402e714db4c79707ac8
oriolclosa <oriol.closa@est.fib.upc.edu>
2018-05-01 00:48:38 +0200

    Experiments

--- main.cc
+++ main.cc
@@ -12,7 +12,7 @@ using namespace std;
 
 int main() {
     ifstream File;
-    File.open("exemples/exemple_01.dict");
+    File.open("exemples/exemple_02.dict");
     if (not File.is_open())
         cout << "Error, no s'ha pogut llegir l'exemple_01.dict." << endl;
     int n;
@@ -20,7 +20,7 @@ int main() {
     vector<int> dict(n), entr(2*n);
     for (int i = 0; i < n; ++i) File >> dict[i];
     File.close();
-    File.open("exemples/exemple_01.entr");
+    File.open("exemples/exemple_02.entr");
     if (not File.is_open())
         cout << "Error, no s'ha pogut llegir l'exemple_01.entr." << endl;
     for (int i = 0; i < 2*n; ++i) File >> entr[i];
